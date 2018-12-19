e3ad805fc4ef9e6d42a6f2b36f1c5ede0d6c60c3
nishanth930 <nishanth930@gmail.com>
2018-12-19 19:22:37 +0530

    19Dec

--- cart.php
+++ cart.php
@@ -22,34 +22,19 @@ defined( 'ABSPATH' ) || exit;
 
 do_action( 'woocommerce_before_cart' ); ?>
 
-<?php
 
-
-$n1="";
-$n2="";
-$n3="";
-
-if(isset($_POST["S1"]))
-{
-$n1=$_POST["T1"];
-$n2=$_POST["T2"];
-$n3=$_POST["T3"];
-
-$r=$n1*$n2*$n3/6000;
-
-}
-
-?>
-<form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post" action="test.php">
+<h1>nishanth</h1>
+<form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
 	<?php do_action( 'woocommerce_before_cart_table' ); ?>
     
 	<table style="width:150%" class="shop_table shop_table_responsive cart woocommerce-cart-form__contents" cellspacing="0">
 		<thead>
 			<tr>
+			<th class="test">nishanth</th> 
 				<th class="product-remove">&nbsp;</th>
 				<th class="product-packagetype"><?php esc_html_e( 'Package Type', 'woocommerce' ); ?></th>
 				<th  ><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
-                <th class="product-length"><?php esc_html_e( 'Length', 'woocommerce' ); ?></th>
+                <th class="product-length"><?php esc_html_e( 'Lenght', 'woocommerce' ); ?></th>
                 <th class="product-width"><?php esc_html_e( 'Width', 'woocommerce' ); ?></th>
                 <th class="product-height"><?php esc_html_e( 'Height', 'woocommerce' ); ?></th>
 				<th class="product-tvw"><?php esc_html_e( 'Total Volumetirc Weigth', 'woocommerce' ); ?></th>
@@ -63,12 +48,14 @@ $r=$n1*$n2*$n3/6000;
 			foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 				$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
 				$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
-
+				
 				if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
 					$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
 					?>
 					<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
-
+					
+<td><?php echo $cart_item['item_length']?> </td>
+				
 						<td class="product-remove">
 							<?php
 								// @codingStandardsIgnoreLine
@@ -89,7 +76,7 @@ $r=$n1*$n2*$n3/6000;
                         <option value="bags">Bags</option>
   						<option value="bales">Bales</option>
   						<option value="boxes">Boxes</option>
-  						<option value="pallet">Pallets</option>
+  						<option value="pallet">Pallets</option>	
 						</select>
 						</td>
 						
@@ -112,13 +99,26 @@ $r=$n1*$n2*$n3/6000;
 						?>
 						</td>
 						<td>
-						<input name="T1" type="text"  class="w3-input w3-border" style="width:50%" value="<?php echo($n1); ?>">
+						<input
+							type="number"
+							id="<?php echo esc_attr( uniqid( 'item_length_' )); ?>"
+							class="input-text item_length text"
+							min="<?php echo esc_attr( $min_value ); ?>"
+							max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
+							name="<?php echo esc_attr( 'item_length' ); ?>"
+							value="<?php echo esc_attr($cart_item['item_length'] ); ?>"
+							title="<?php echo esc_attr_x( 'item_length', 'Product quantity input tooltip', 'woocommerce' ); ?>"
+							size="4"
+							pattern="<?php echo esc_attr( $pattern ); ?>"
+							inputmode="<?php echo esc_attr( 'numeric' ); ?>"
+							/>
+						
 						</td>
 						<td>
-						<input name="T2" type="text" class="w3-input w3-border" style="width:50%" value="<?php echo($n2); ?>">
+						<input name="item_width" type="text" class="w3-input w3-border" style="width:50%" value="<?php echo($n2); ?>">
 						</td>
 						<td>
-						<input name="T3" type="text" class="w3-input w3-border" style="width:50%" value="<?php echo($n3); ?>">
+						<input name="item_height" type="text" class="w3-input w3-border" style="width:50%" value="<?php echo($n3); ?>">
 						</td>
 
 
@@ -151,7 +151,7 @@ $r=$n1*$n2*$n3/6000;
 						</div>
 					<?php } ?>
 
-					<button type="submit" name="S1"class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
+					<button type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
 
 					<?php do_action( 'woocommerce_cart_actions' ); ?>
 
@@ -179,3 +179,5 @@ $r=$n1*$n2*$n3/6000;
 </div>
 
 <?php do_action( 'woocommerce_after_cart' ); ?>
+
+
